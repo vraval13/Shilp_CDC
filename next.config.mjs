d7c70ecc7a630d85421573cb54b52753af7f24bf
Vyom Raval <vraval13@Vyoms-MacBook-Air-2.local>
2026-01-25 11:55:22 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,6 @@ const nextConfig = {
   compress: true,
   poweredByHeader: false,
   productionBrowserSourceMaps: false,
-  swcMinify: true,
   reactStrictMode: true,
   headers: async () => {
     return [
